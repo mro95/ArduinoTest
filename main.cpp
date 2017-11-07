cd5ffad7f7de5bd4df3d3e5bf2603be3de5e3456
Maarten Oosting <mail@maartenoosting.nl>
2017-11-07 12:01:29 +0100

    oops

--- main.cpp
+++ main.cpp
@@ -1,44 +1,80 @@
 #include <avr/io.h>
-#include <avr/interrupt.h>
+#include <stdlib.h>
+#include <avr/sfr_defs.h>
+#define F_CPU 16E6
+#include <util/delay.h>
+// output on USB = PD1 = board pin 1
+// datasheet p.190; F_OSC = 16 MHz & baud rate = 19.200
+#define UBBRVAL 51
 
-bool isOn = false;
-uint8_t cnt = 0;
 
-void ledOn() {
-    PINB |= (1 << 5);
+void uart_init()
+{
+    // set the baud rate
+    UBRR0H = 0;
+    UBRR0L = UBBRVAL;
+    // disable U2X mode
+    UCSR0A = 0;
+    // enable transmitter
+    UCSR0B = _BV(TXEN0);
+    // set frame format : asynchronous, 8 data bits, 1 stop bit, no parity
+    UCSR0C = _BV(UCSZ01) | _BV(UCSZ00);
+}
+void transmit(uint8_t data)
+{
+    // wait for an empty transmit buffer
+    // UDRE is set when the transmit buffer is empty
+    loop_until_bit_is_set(UCSR0A, UDRE0);
+    // send the data
+    UDR0 = data;
 }
 
-void ledOff() {
-    PINB &= ~(1 << 5);
+uint8_t readAnalog(uint8_t ch)
+{
+    // select the corresponding channel 0~7
+    // ANDing with ’7′ will always keep the value
+    // of ‘ch’ between 0 and 7
+    ch &= 0b00000111;  // AND operation with 7
+    ADMUX = (ADMUX & 0xF8)|ch; // clears the bottom 3 bits before ORing
+ 
+    // start single convertion
+    // write ’1′ to ADSC
+    ADCSRA |= (1<<ADSC);
+ 
+    // wait for conversion to complete
+    // ADSC becomes ’0′ again
+    // till then, run loop continuously
+    while(ADCSRA & (1<<ADSC));
+ 
+    return (ADC);
 }
 
-ISR(TIMER0_OVF_vect){
-    cnt++;
-    if (cnt >= 61/2) {
-        if (isOn) {
-            ledOff();
-            isOn = false;
-        } else {
-            ledOn();
-            isOn = true;
-        }
-        cnt = 0;
+void setup() {
+     // AREF = AVcc
+    ADMUX = (1<<REFS0);
+ 
+    // ADC Enable and prescaler of 128
+    // 16000000/128 = 125000
+    ADCSRA = (1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
+
+    // Setup uart
+    uart_init();
+
+    // Wait 1 second for setup uart
+    _delay_ms(1000);
+}
+
+void loop() {
+    uint8_t reading = readAnalog(0);
+    transmit(reading); 
+    _delay_ms(500);
+}
+
+int main(void) 
+{
+    setup();
+    while (1) {
+        loop();
     }
-}
-
-int main() {
-    DDRB |= (1 << 5);
-
-    TCCR0B |= (1 << CS00) | (1 << CS02);
-
-    TCNT0 = 0;
-
-    TIMSK0 |= (1 << TOIE0);
-
-    sei();
-
-
-
-    while(true) {};
 }
 
